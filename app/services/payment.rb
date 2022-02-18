ad387d3b427100e44c1b7ef3b9d57aa2257aad72
Mukesh Patel <mukeshkup92@gmail.com>
2022-02-18 16:31:18 +0530

    quick change

--- app/services/payment.rb
+++ app/services/payment.rb
@@ -3,7 +3,7 @@ class Payment
     def self.status(order_id)
       order = Order.find(order_id)
       order.status = ["paid", "unpaid"].sample
-      orer.paid_at = Date.today if order.status == "paid"
+      order.paid_at = Date.today if order.status == "paid"
       order.save
     end
 end
